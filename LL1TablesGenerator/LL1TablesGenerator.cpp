cd0e55a59b8e8f652d0c685f5d982be31c8c99e5
Sasha Ocheev <OcheevSasha@gmail.com>
2017-01-16 23:45:02 +0300

    working arithmetic grammar without start sets

--- LL1TablesGenerator/LL1TablesGenerator.cpp
+++ LL1TablesGenerator/LL1TablesGenerator.cpp
@@ -15,95 +15,81 @@ std::pair<CToken, std::vector<std::vector<CToken>>> MakeGrammarLine(const std::s
 }
 
 
-int main()
+typedef std::vector<std::pair<CToken, std::vector<std::vector<CToken>>>> GrammarType;
+
+GrammarType GetExampleIniziateDirectly()
 {
-	std::vector<std::pair<CToken, std::vector<std::vector<CToken>>>> grammar =
+	GrammarType grammar =
 	{
-		MakeGrammarLine("PROG", { { CToken("PROG", Token::TERMINAL), CToken("id", Token::TERMINAL), CToken("VAR", Token::NONTERMINAL), CToken("begin", Token::TERMINAL), CToken("lists", Token::NONTERMINAL), CToken("END", Token::TERMINAL), }, }),
-		MakeGrammarLine("VAR", { { CToken("VAR", Token::TERMINAL), CToken("idlist", Token::NONTERMINAL) } }),
-		MakeGrammarLine("idlist", { { CToken("id", Token::TERMINAL), CToken("A", Token::NONTERMINAL) } }),
-		MakeGrammarLine("A", { { CToken(",", Token::TERMINAL), CToken("idlist", Token::NONTERMINAL) }, { CToken("") }, }),
-		MakeGrammarLine("lists",  { { CToken("st", Token::TERMINAL), CToken("B", Token::NONTERMINAL) } }),
-		MakeGrammarLine("B", { { CToken("lists", Token::NONTERMINAL) }, { CToken("") } }),
-		/*MakeGrammarLine("sign-expr",{
-			{ CToken("sign", Token::NONTERMINAL), CToken("expr", Token::NONTERMINAL) }
-		}),
-		MakeGrammarLine("expr",{
-			{ CToken("paren", Token::NONTERMINAL), CToken("after", Token::NONTERMINAL) },
-			{ CToken("type", Token::NONTERMINAL), CToken("after", Token::NONTERMINAL) }
-		}),
-		MakeGrammarLine("after",{
-			{ CToken("act", Token::NONTERMINAL), CToken("after-act", Token::NONTERMINAL) },
-			{ CToken("") } 
-		}),
-		MakeGrammarLine("after-act",{
-			{ CToken("paren", Token::NONTERMINAL) },
-			{ CToken("expr", Token::NONTERMINAL) }
-		}),
-		MakeGrammarLine("paren",{
-			{ CToken("(", Token::TERMINAL), CToken("sign-expr", Token::NONTERMINAL), CToken(")", Token::TERMINAL) }
-		}),
-		MakeGrammarLine("act",{
-			{ CToken("+", Token::TERMINAL) },
-			{ CToken("-", Token::TERMINAL) },
-			{ CToken("*", Token::TERMINAL) },
-			{ CToken("/", Token::TERMINAL) },
-			{ CToken("%", Token::TERMINAL) },
-		}),
-		MakeGrammarLine("type",{
-			{ CToken("int", Token::TERMINAL) },
-			{ CToken("id", Token::NONTERMINAL) },
-			{ CToken("float", Token::TERMINAL) }
-		}),
-		MakeGrammarLine("id",{
-			{ CToken("id", Token::TERMINAL), CToken("idx", Token::NONTERMINAL) }
-		}),
-		MakeGrammarLine("idx",{
-			{ CToken("[", Token::TERMINAL), CToken("after-idx", Token::NONTERMINAL) },
-			{ CToken("") }
-		}),
-		MakeGrammarLine("after-idx",{
-			{ CToken("int", Token::TERMINAL), CToken("]", Token::TERMINAL) },
-			{ CToken("id", Token::TERMINAL), CToken("]", Token::TERMINAL) },
-		}),
-		MakeGrammarLine("sign",{
-			{ CToken("+", Token::TERMINAL) },
-			{ CToken("-", Token::TERMINAL) },
-			{ CToken("") }
-		}),*/
+		MakeGrammarLine("PROG",{ { CToken("PROG", Token::TERMINAL), CToken("id", Token::TERMINAL), CToken("VAR", Token::NONTERMINAL), CToken("begin", Token::TERMINAL), CToken("lists", Token::NONTERMINAL), CToken("END", Token::TERMINAL), }, }),
+		MakeGrammarLine("VAR",{ { CToken("VAR", Token::TERMINAL), CToken("idlist", Token::NONTERMINAL) } }),
+		MakeGrammarLine("idlist",{ { CToken("id", Token::TERMINAL), CToken("A", Token::NONTERMINAL) } }),
+		MakeGrammarLine("A",{ { CToken(",", Token::TERMINAL), CToken("idlist", Token::NONTERMINAL) },{ CToken("") }, }),
+		MakeGrammarLine("lists",{ { CToken("st", Token::TERMINAL), CToken("B", Token::NONTERMINAL) } }),
+		MakeGrammarLine("B",{ { CToken("lists", Token::NONTERMINAL) },{ CToken("") } }),
 	};
 
-	grammar.clear();
-	MakeGrammarLine("PROG", { { CToken("PROG", Token::TERMINAL), CToken("id", Token::TERMINAL), CToken("VAR", Token::NONTERMINAL), CToken("begin", Token::TERMINAL), CToken("lists", Token::NONTERMINAL), CToken("END", Token::TERMINAL), }, }),
-		MakeGrammarLine("VAR", { { CToken("VAR", Token::TERMINAL), CToken("idlist", Token::NONTERMINAL) } }),
-		MakeGrammarLine("idlist", { { CToken("id", Token::TERMINAL), CToken("A", Token::NONTERMINAL) } }),
-		MakeGrammarLine("A", { { CToken(",", Token::TERMINAL), CToken("idlist", Token::NONTERMINAL) },{ CToken("") }, }),
-		MakeGrammarLine("lists", { { CToken("st", Token::TERMINAL), CToken("B", Token::NONTERMINAL) } }),
-		MakeGrammarLine("B", { { CToken("lists", Token::NONTERMINAL) },{ CToken("") } }),
-	grammar = InitGrammar({
-		{"<PROG>", ":", "PROG", "id", "<VAR>", "begin", "<lists>", "end"},
-		{"<VAR>", ":", "VAR", "<idlist>"},
-		{"<idlist>", ":","id", "<A>"},
-		{"<A>", ":", ",", "<idlist>", "|", ""},
-		{"<lists>", ":", "st", "<B>"},
-		{"<B>", ":", "<lists>", "|", ""}
+	return grammar;
+}
+
+GrammarType GetExampleByStringVectorVector()
+{
+	GrammarType grammar = InitGrammar({
+		{ "<PROG>", ":", "PROG", "id", "<VAR>", "begin", "<lists>", "end" },
+		{ "<VAR>", ":", "VAR", "<idlist>" },
+		{ "<idlist>", ":","id", "<A>" },
+		{ "<A>", ":", ",", "<idlist>", "|", "" },
+		{ "<lists>", ":", "st", "<B>" },
+		{ "<B>", ":", "<lists>", "|", "" }
 	});
-	grammar.clear();
-	grammar = InitGrammar({
+
+	return grammar;
+}
+
+GrammarType GetExampleByStringVector()
+{
+	GrammarType grammar = InitGrammar({
 		"<PROG> : PROG id <VAR> begin <lists> end",
-		 "<VAR> : VAR <idlist>" ,
-		 "<idlist> : id <A>" ,
-		 "<A> : , <idlist> | <>" ,
-		 "<lists> : st <B>" ,
-		 "<B> : <lists> | <>" 
+		"<VAR> : VAR <idlist>" ,
+		"<idlist> : id <A>" ,
+		"<A> : , <idlist> | <>" ,
+		"<lists> : st <B>" ,
+		"<B> : <lists> | <>"
 	});
+
+	return grammar;
+}
+
+
+GrammarType GetArithmetics()
+{
+	GrammarType grammar = InitGrammar({
+		"<sign-expr> : <sign> <expr>",
+		"<expr> : <paren> <after> | <type> <after>",
+		"<after> : <act> <expr> | <>",
+		"<paren> : ( <sign-expr> )",
+		"<act> : / | * | % | * | - | +",
+		"<type> : int | float | <id>",
+		"<id> : id <idx>",
+		"<idx> : [ <after-idx>",
+		"<after-idx> : int ] | id ]",
+		"<sign> : + | - | <>"
+	});
+
+	return grammar;
+}
+
+
+int main()
+{
+	auto grammar = GetArithmetics();
 
 	CTableGenerator table(grammar);
 	auto t = table.GetTable();
 	std::ofstream file;
 	file.open("table.txt");
-	//PrintTableForC(file, t);
-	PrintTable(file, t);
+	PrintTableForC(file, t);
+	//PrintTable(file, t);
 	file.close();
 
 
